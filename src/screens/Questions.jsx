eaf9f4d21532f7fb6dfa06632fd98c58af553154
Raju Prasad <rp0071085@gmail.com>
2025-02-01 13:05:28 +0530

    done

--- src/screens/Questions.jsx
+++ src/screens/Questions.jsx
@@ -39,20 +39,17 @@ const Questions = () => {
 
     if (result) {
       setIsCorrect(true);
-      setPoints(points + 4);
       console.log("Correct!");
-      console.log(points);
+      setPoints((prevPoints) => prevPoints + 4);
     } else {
       setIsCorrect(false);
-      setPoints(points - 1);
-      console.log(points);
-      // Decrease points by 1 for incorrect answer
+      setPoints((prevPoints) => prevPoints - 1); // Decrease points by 1 for incorrect answer
     }
   };
 
   const handleNextQuestion = () => {
     if (currentQuestion < question.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
+      setCurrentQuestion((prevCurrentQuestion) => prevCurrentQuestion + 1);
       setIsOptionSelected(false); // Reset option selection for the next question
       setSelectedAnswer(null); // Reset selected answer for the next question
     } else {
@@ -63,63 +60,75 @@ const Questions = () => {
   };
 
   return (
-    <div className="container mx-auto p-8 bg-slate-600 min-h-screen flex justify-center items-center">
+    <div className="container mx-auto p-8 bg-slate-600 min-h-screen flex flex-col justify-center items-center">
       {loading ? (
         <div className="flex justify-center items-center h-screen flex-col">
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
           <div className="text-4xl font-semibold my-4">BEST OF LUCK❤️</div>
         </div>
       ) : (
-        <div className="rounded-lg shadow-md p-6 bg-slate-400 my-4 w-full">
-          <h2 className="text-2xl font-semibold mb-4 text-gray-800 my-4">
-            Question. {question[currentQuestion].description}
-          </h2>
-          <div className="flex flex-col gap-4 px-4">
-            {question[currentQuestion].options.map((option, index) => (
-              <div
-                key={index}
-                onClick={() => handleOptionClick(option)}
-                className={`rounded-lg p-4 cursor-pointer transition duration-300 ${
-                  selectedAnswer === option
-                    ? option.is_correct
-                      ? "bg-green-300"
-                      : "bg-red-300"
-                    : "bg-gray-100 hover:bg-gray-200"
-                }`}
-              >
-                <p className="text-lg text-gray-700">{option.description}</p>
-              </div>
-            ))}
+        <>
+          <div className="w-full flex justify-center mb-4">
+           
           </div>
-          {isOptionSelected && (
-            <div className="flex justify-end mt-4">
-              {currentQuestion < question.length - 1 ? (
-                <button
-                  onClick={handleNextQuestion}
-                  className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-                >
-                  Next
-                </button>
-              ) : (
-                <button onClick={handleNextQuestion} >
-                <Link
-                
-                  to={{
-                    pathname: "/result",
-                    state: { points },
-                  }}
-                  className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-                >
-                  Result
-                </Link>
-                </button>
-              )}
+          <div className="rounded-lg shadow-md p-6 bg-slate-400 my-4 w-full">
+          <div className="w-full flex">
+              {Array.from({ length: 10 }).map((_, index) => (
+                <div
+                  key={index}
+                  className={`flex-1 h-1 mx-1 rounded ${
+                    index < currentQuestion ? "bg-green-800" : "bg-gray-300"
+                  }`}
+                ></div>
+              ))}
             </div>
-          )}
-        </div>
+            <h2 className="text-2xl font-semibold mb-4 text-gray-800 my-4">
+              Question {currentQuestion + 1}. {question[currentQuestion].description}
+            </h2>
+            <div className="flex flex-col gap-4 px-4">
+              {question[currentQuestion].options.map((option, index) => (
+                <div
+                  key={index}
+                  onClick={() => handleOptionClick(option)}
+                  className={`rounded-lg p-4 cursor-pointer transition duration-300 ${
+                    selectedAnswer === option ? (option.is_correct ? "bg-green-300" : "bg-red-300") : "bg-gray-100 hover:bg-gray-200"
+                  }`}
+                >
+                  <p className="text-lg text-gray-700">{option.description}</p>
+                </div>
+              ))}
+            </div>
+            {isOptionSelected && (
+              <div className="flex justify-end mt-4">
+                {currentQuestion < question.length - 1 ? (
+                  <button
+                    onClick={handleNextQuestion}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+                  >
+                    Next
+                  </button>
+                ) : (
+                  <button
+                    onClick={handleNextQuestion} >
+                  <Link
+                    to={{
+                      pathname: "/result",
+                      state: { points },
+                    }}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+                  >
+                    Result
+                  </Link>
+                  </button>
+                )}
+              </div>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
 };
 
 export default Questions;
+
